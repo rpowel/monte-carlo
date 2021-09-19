aca41f95f012ddc602e84b69f6162816d6fc2218
Robert Powel <48132667+rpowel@users.noreply.github.com>
2021-09-19 17:56:57 -0400

    Reworked profiler - profiler.py now can generate time curves for different number of cores used - changed defaults in profiler_defaults.json to more useful values

--- src/profiler.py
+++ src/profiler.py
@@ -24,46 +24,37 @@ def calc_relative_err(ideal_value, calculated_list):
     return abs(calculated_list - ideal_value) / ideal_value
 
 
-def profile_num_iterations(func, **kwargs):
-    iter_list = []
-    value_list = []
-    time_list = []
-    num_iterations = kwargs.get('iter_low', DEFAULT_VALUES['iter_low'])
-    while num_iterations < kwargs.get('iter_high', DEFAULT_VALUES['iter_high']):
-        iter_list.append(num_iterations)
-        value, time_ = time_function(func, num_iterations=num_iterations)
-        value_list.append(value)
-        time_list.append(time_)
-        num_iterations *= 10
-    iter_list = np.asarray(iter_list)
-    value_list = np.asarray(value_list)
-    time_list = np.asarray(time_list)
-    return iter_list, value_list, time_list
-
-
 def profile_num_cores(func, **kwargs):
-    iter_list = []
-    value_list = []
-    time_list = []
-    num_cores = kwargs.get('cores_low', DEFAULT_VALUES['cores_low'])
-    while num_cores < kwargs.get('cores_high', DEFAULT_VALUES['cores_high']):
-        iter_list.append(num_cores)
-        value, time_ = time_function(func, num_cores=num_cores)
-        value_list.append(value)
-        time_list.append(time_)
-        num_cores += 1
+    iter_low = kwargs.get('iter_low', DEFAULT_VALUES['iter_low'])
+    iter_high = kwargs.get('iter_high', DEFAULT_VALUES['iter_high'])
+    num_iter_list = np.logspace(
+        int(np.log10(iter_low)),
+        int(np.log10(iter_high)),
+        num=2*int((np.log10(iter_high) - np.log10(iter_low))),
+        dtype=int,
+    )
+    cores_low = kwargs.get('cores_low', DEFAULT_VALUES['cores_low'])
+    cores_high = kwargs.get('cores_high', DEFAULT_VALUES['cores_high'])
+    num_cores_list = np.arange(cores_low, cores_high+1, 1, dtype=int)
 
-    iter_list = np.asarray(iter_list)
-    value_list = np.asarray(value_list)
-    time_list = np.asarray(time_list)
-    return iter_list, value_list, time_list
+    values_list = np.zeros([num_cores_list.size, num_iter_list.size])
+    times_list = np.zeros([num_cores_list.size, num_iter_list.size])
+
+    for i, num_cores in enumerate(num_cores_list):
+        for j, num_iterations in enumerate(num_iter_list):
+            values_list[i, j], times_list[i, j] = time_function(
+                func,
+                num_iterations=num_iterations,
+                num_cores=num_cores,
+            )
+
+    return num_iter_list, num_cores_list, values_list, times_list
 
 
-def plot_err_curve(ideal_value, ind_var_list, value_list, time_list, **kwargs):
+def plot_err_curve(ideal_value, ind_var_list, value_list, **kwargs):
     err_list = calc_relative_err(ideal_value, value_list)
 
     plt.loglog(ind_var_list, err_list, c='C0', alpha=0.7, label='Monte-Carlo')
-
     plt.xlabel('Number of Iterations')
     plt.ylabel('Relative Error')
     plt.tight_layout()
@@ -71,26 +62,40 @@ def plot_err_curve(ideal_value, ind_var_list, value_list, time_list, **kwargs):
     plt.show()
 
 
-def gen_profile_curves(ideal_value, func_to_integrate, **kwargs):
-    num_iter_list, value_iter_list, time_iter_list = profile_num_iterations(
-        func_to_integrate,
-        iter_high=kwargs.get('iter_high', DEFAULT_VALUES['iter_high']),
-        iter_low=kwargs.get('iter_low', DEFAULT_VALUES['iter_low']),
-        num_cores=kwargs.get('num_cores', DEFAULT_VALUES['num_cores']),
+def plot_single_time_curve(num_iter_list, time_list, num_cores, **kwargs):
+    plt.plot(num_iter_list, time_list, label=f'{num_cores} Cores')
+
+
+def plot_all_time_curves(num_iter_list, time_list, num_cores_list, **kwargs):
+    for i, (times, num_cores) in enumerate(zip(time_list, num_cores_list)):
+        plot_single_time_curve(num_iter_list, times, num_cores)
+
+    plt.xscale('log')
+    plt.grid(alpha=0.4)
+
+    plt.ylabel(kwargs.get('ylabel', 'Time (s)'))
+    plt.xlabel(kwargs.get('xlabel', 'Number of Iterations'))
+    plt.title(kwargs.get('title', 'Calculation Time'))
+
+    plt.tight_layout()
+    plt.legend(loc=0)
+    plt.show()
+
+
+def gen_cores_profile_plots(ideal_value, func_to_integrate, **kwargs):
+    num_iter_list, num_cores_list, values_list, times_list = profile_num_cores(func_to_integrate, **kwargs)
+    plot_all_time_curves(
+        num_iter_list,
+        times_list,
+        num_cores_list,
+        title=f"Calculation Time vs. Number of Cores Used",
+        xlabel="Number of Iterations",
+        ylabel="Time (s)",
     )
-    print(value_iter_list)
-    plot_err_curve(ideal_value, num_iter_list, value_iter_list, time_iter_list)
-    num_cores_list, value_cores_list, time_cores_list = profile_num_cores(
-        func_to_integrate,
-        cores_low=kwargs.get('cores_low', DEFAULT_VALUES['cores_low']),
-        cores_high=kwargs.get('cores_high', DEFAULT_VALUES['cores_high']),
-        num_iterations=kwargs.get('num_iterations', DEFAULT_VALUES['num_iterations']),
-    )
-    plot_err_curve(ideal_value, num_cores_list, value_cores_list, time_cores_list)
 
 
 def main():
-    gen_profile_curves(np.pi, calculate_pi, num_cores=10, iter_high=1e10)
+    gen_cores_profile_plots(np.pi, calculate_pi, cores_low=4, cores_high=10, iter_low=100, iter_high=1e7)
 
 
 if __name__ == "__main__":
